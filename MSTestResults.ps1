6e2a6ace35f4b2698f98a79c8453a9cdba7f4a6b
Bert <bert.k@hotmail.com>
2017-07-08 11:19:26 +0200

    rename files

---
+++ MSTestResults.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+   [string]$TestResults
+)
+
+[string] $mypath = Get-Location
+$Dir = get-childitem $mypath -recurse 
+$List = $Dir | where {$_.extension -eq ".trx"} |Get-Unique 
+
+if ($List -eq $null) {
+  Write-Output  "Cannot find a '.trx' file using path '$mypath'"
+} else {
+  [string] $FileName = "$mypath\MSTestResults.trx"
+  if (Test-Path $FileName) {
+    Remove-Item $FileName
+  }    
+  Write-Output "Create $FileName"
+  Copy-Item $List.FullName $FileName
+}
+
