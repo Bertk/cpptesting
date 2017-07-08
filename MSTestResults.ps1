b56973718975da9de6e67e0c37abc6408d21dabe
Bert <bert.k@hotmail.com>
2017-07-08 11:43:56 +0200

    fix paths

--- MSTestResults.ps1
+++ MSTestResults.ps1
@@ -3,16 +3,16 @@
 )
 
 [string] $mypath = Get-Location
-$Dir = get-childitem $mypath -recurse 
+$Dir = get-childitem "$mypath\TestResults" -recurse 
 $List = $Dir | where {$_.extension -eq ".trx"} |Get-Unique 
 
 if ($List -eq $null) {
-  Write-Output  "Cannot find a '.trx' file using path '$mypath'"
+  Write-Output  "Cannot find a '.trx' file using path '$mypath\TestResults'"
 } else {
   [string] $FileName = "$mypath\MSTestResults.trx"
   if (Test-Path $FileName) {
     Remove-Item $FileName
-  }    
+  } 
   Write-Output "Create $FileName"
   Copy-Item $List.FullName $FileName
 }
