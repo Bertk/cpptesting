ebb8431e813b5faa4338f438128e5063b7c497af
Bert <bert.k@hotmail.com>
2017-07-16 16:45:27 +0200

    fix folder names

--- Tests/RootFinderTests/stdafx.cpp
+++ Tests/RootFinderTests/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// SampleTest1.pch will be the pre-compiled header
+// RootFinderTests.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
