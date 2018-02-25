caa84ffe0aef8263614ec9d6866091878a76de58
Bert <bert.k@hotmail.com>
2018-02-25 18:54:39 +0100

    add test configuration

--- Source/Calculation/Calculation.cpp
+++ Source/Calculation/Calculation.cpp
@@ -4,18 +4,17 @@
 #include "Multiply.h" 
 #include <stdio.h> 
 
-int main()
-
-{
-        const int x = 4;
-        const int y = 5;
-
-        const int z1 = CAddition::twoValues(x, y);
-        printf("\nAddition Result: %d\n", z1);
-
-        const int z2 = CMultiply::twoValues(x, y);
-        printf("Multiply Result: %d\n", z2);
-
-//        delete corporation;
-        return 0;
-}
+//int main()
+//
+//{
+//        const int x = 4;
+//        const int y = 5;
+//
+//        const int z1 = CAddition::twoValues(x, y);
+//        printf("\nAddition Result: %d\n", z1);
+//
+//        const int z2 = CMultiply::twoValues(x, y);
+//        printf("Multiply Result: %d\n", z2);
+//
+//        return 0;
+//}
